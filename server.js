2c384d41c50051aca77b5fd62098ce83856a9cdf
ashartahir <ashartahir786@gmail.com>
2026-01-18 04:33:01 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
